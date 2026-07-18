4de45f86f2b0524490b23677afc178117ec2b9bf
nasirk007 <nsr.capital.007@outlook.com>
2026-07-18 03:02:08 +0200

    find missing value in dataframe which is structured tabular form for messy csv data, then fund position of any missing value in DF, position means x, y coordinate of each value, however each cell under different columns had different datatype, convert them first into boolen and from boolen to numeric using  numpy, as plot in matplotlib library require numeric values for visualisation of data manipulation and analysis, numpy used for numerical operation and for 2D data, pandas convert data into tabluer frame and allow manipluation, while matplotlib enable visualisation of such analysis and manipulation

---
+++ data_analysis/data_preparation_visualization/1-plot_missingness.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""visualise missing values in pandas dataframe"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_missingness(df):
+    """This task isto plot missing value in datafram."""
+    plt.figure(figsize=(12, 8))
+    # find missing val and location of missing_value
+    # df.isna() results boolen of missing value while
+    # np.where will convert them into array of index
+    # numpy examine 2D structures only, and return array
+    # simply combining both np.where and df.column provides
+    # x and y coordinates to be used by matplot for visualisation
+    missing_row, missing_col = np.where(df.isna())
+    # first step isto plot missing value on x and y
+    plt.scatter(missing_row, missing_col, marker="|")
+    plt.yticks(range(len(df.columns)), df.colummns)
+    plt.title("Missingness Plot")
+    plt.tight_layout()
+    plt.show()
+    return None
