0616cc13324d12e01f095d3a1f28c89c5dd92bda
nasirk007 <nsr.capital.007@outlook.com>
2026-07-19 03:04:39 +0200

    removing duplicate rows, duplicated turn column into row by default and remove duplicate row in df

---
+++ data_analysis/data_preparation_visualization/4-remove_duplicates.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""this module handle duplicates rows in DF"""
+import pandas as pd
+
+
+def remove_duplicates(df):
+    """Task to remove duplicate rows from DF."""
+    # duplicate method by default turn column into row and check duplicates
+    # but if you want to check duplicates by column, use transpose before
+    # duplicate function e.g df.T.duplicated()
+    df = df.drop_duplicates()
+    return df
