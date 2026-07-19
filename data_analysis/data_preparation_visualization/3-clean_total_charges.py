314bed83c58c28877c0f255a6f8261850a974769
nasirk007 <nsr.capital.007@outlook.com>
2026-07-19 02:29:14 +0200

    drop, replace, reasonable guess for replacing missing value NaN, this task only work when we have numeric value in totalcharges col, and thats why we import covert col function from task_2, once we have numeric values in this col, we can use dropna fillna method and both expect integer/float or numeric data type values rather object or string

---
+++ data_analysis/data_preparation_visualization/3-clean_total_charges.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""visualise convert column datatype dataframe"""
+import pandas as pd
+
+
+def def clean_total_charges(df, method='drop'):
+    """Task to do modification 4 missing values in particular column of DF."""
+    # to see output of below, we need to have object
+    # like what is df, like we need to read csv
+    # after df.info() tell that datatype of info sitting under each col
+    # make sure, those data type make sense otherwise convert them into
+    # other suitable data type, as required for analysis
+    # dropna will remove rows with NaN rather empty in totalcharges
+    # to get NaN, we need to convert this col to numeric becuase df.info()
+    # told us this col contain string type data (object or "O")
+    # this change is done at DF level for particular column
+    # means all those row from dataframe will be remove based on
+    # if cell are empty in one particular columns like totalcharges
+    if method == "drop":
+        df = df.dropna(subset=['TotalCharges'])
+    elif method == "median":
+        # this change is only for particular col, so stored in that col only
+        df["TotalCharges"] = df["TotalCharges"].fillna(df[
+            "TotalCharges"].median())
+    elif method == "impute":
+        df["TotalCharges"] = df["TotalCharges"].fillna(df[
+            "MonthlyCharges"] * ["tenure"])
+    return df
