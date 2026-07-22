91ff17ee185462554d96603e6e3a7976e43ca992
nasirk007 <nsr.capital.007@outlook.com>
2026-07-22 13:33:56 +0200

    final update due to reindex() function

--- data_analysis/data_preparation_visualization/6-plot_churn_distribution.py
+++ data_analysis/data_preparation_visualization/6-plot_churn_distribution.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 def plot_churn_distribution(df):
     """the function will plot customer churn data"""
     plt.figure(figsize=(12, 8))
-    counts = df["Churn"].value_counts().reindex(["No", "Yes"])
-    plt.bar(counts.index, counts.values, color=["skyblue", "salmon"])
+    count = df["Churn"].value_counts().reindex(["Yes", "No"])
+    plt.bar(count.index, count.values, color=["skyblue", "salmon"])
     plt.ylabel("Count")
     plt.title("Churn_Distribution")
     plt.show()
