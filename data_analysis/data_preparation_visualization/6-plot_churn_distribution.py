21c36f2081f4167c1252d9f06143bbd52a29868a
nasirk007 <nsr.capital.007@outlook.com>
2026-07-22 14:42:44 +0200

    final update due to reindex() function

--- data_analysis/data_preparation_visualization/6-plot_churn_distribution.py
+++ data_analysis/data_preparation_visualization/6-plot_churn_distribution.py
@@ -1,32 +1,14 @@
 #!/usr/bin/env python3
-"""
-This module visualizes churn class distribution in a DataFrame
-"""
+"""This module visualizes churn class distribution in a DataFrame"""
 import matplotlib.pyplot as plt
 
 
 def plot_churn_distribution(df):
-    """
-    Plots the distribution of the Churn column.
-
-    Args:
-        df: The pandas DataFramecontaining a Churn column.
-
-    Returns:
-        None
-    """
+    """Plots the distribution of the Churn column."""
     plt.figure(figsize=(12, 8))
-
     counts = df["Churn"].value_counts().reindex(["No", "Yes"])
-
-    plt.bar(
-        counts.index,
-        counts.values,
-        color=["skyblue", "salmon"]
-        )
-
+    plt.bar(counts.index, counts.values, color=["skyblue", "salmon"])
     plt.title("Churn Distribution")
     plt.ylabel("Count")
     plt.show()
-
     return None
