e39869e1d43e1e01e5935e675509101aeda33f2f
nasirk007 <nsr.capital.007@outlook.com>
2026-07-22 13:17:29 +0200

    count_value in column, using index approach for bar plot x-axis parameter and value on y-axis. I was filtering whole table based on churn column yes and no, and then was counting yes or no, but this apporach does not fit into bar plot syntax. accordingly i used count_value()/column approach directly

---
+++ data_analysis/data_preparation_visualization/6-plot_churn_distribution.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""this module is to visulize churn class distribution"""
+import matplotlib.pyplot as plt
+
+
+def plot_churn_distribution(df):
+    """the function will plot customer churn data"""
+    plt.figure(figsize=(12, 8))
+    count = df["Churn"].value_counts()
+    plt.bar(count.index, count.values, color=["skyblue", "salmon"])
+    plt.xlabel("Churn")
+    plt.ylabel("Count")
+    plt.title("Churn_Distribution")
+    plt.show()
+    return None
