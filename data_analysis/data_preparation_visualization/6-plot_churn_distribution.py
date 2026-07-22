d482fbb55c918f5040796a8030a9e540e529e0dc
nasirk007 <nsr.capital.007@outlook.com>
2026-07-22 13:28:32 +0200

    no need to xlable, as its not required while no and yes on x-axis are not lable rather unique values thrown by valaue_count() function on x-axis

--- data_analysis/data_preparation_visualization/6-plot_churn_distribution.py
+++ data_analysis/data_preparation_visualization/6-plot_churn_distribution.py
@@ -6,9 +6,8 @@ import matplotlib.pyplot as plt
 def plot_churn_distribution(df):
     """the function will plot customer churn data"""
     plt.figure(figsize=(12, 8))
-    count = df["Churn"].value_counts().reindex(["No", "Yes"])
-    plt.bar(count.index, count.values, color=["skyblue", "salmon"])
-    plt.xlabel("Churn")
+    counts = df["Churn"].value_counts().reindex(["No", "Yes"])
+    plt.bar(counts.index, counts.values, color=["skyblue", "salmon"])
     plt.ylabel("Count")
     plt.title("Churn_Distribution")
     plt.show()
