278390177aad1760906ba8c03e60210c72258334
nasirk007 <nsr.capital.007@outlook.com>
2026-07-24 13:10:46 +0200

    notes for numerical col plot using histogram, KDE curve and Box plot

--- data_analysis/data_preparation_visualization/8-plot_continuous_distributions.py
+++ data_analysis/data_preparation_visualization/8-plot_continuous_distributions.py
@@ -18,7 +18,7 @@ def plot_continuous_distributions(df, columns_to_plot=None):
             if df[col].dtype in ["int64", "float64"]:
                 columns_to_plot.append(col)
     # set up the grid and this n_cols variable isto
-    # setup now of row rather column and this clarified
+    # setup now of row of grid rather column and this clarified
     # from syntax of subplot()
     n_cols = len(columns_to_plot)
     fig, axes = plt.subplots(n_cols, 2, figsize=(10, 3*n_cols))
@@ -35,7 +35,18 @@ def plot_continuous_distributions(df, columns_to_plot=None):
         # in any condition
 
     for i, column in enumerate(columns_to_plot):
+        # loop through each numerical column using its index and name
+        # pick one column under loop, check it first whether it contain missing value
+        # remove NaN/None value from that particular columns, this is to make sure
+        # subplot does not have gap, meams consistently bar appears to reflect data in them 
         data = df[column].dropna()
+        # create histogram on left side, as axes is subplot and i=rowofgrid
+        # while j=colofgrid, and here there are 2 col in grid, 0 represent first
+        # col which will on left while 1 will represent col on right side of grid
+        # create 30 bars from data, data is one numeric column each time loop will run
+        # through from start to end, dansity is simply count/frequency/dansity/probability of 
+        # continous values in one particular numerical columns, Alpha make bars transparent,
+        # while boarder will be colored black
         axes[i, 0].hist(
             data,
             bins=30,
@@ -43,6 +54,15 @@ def plot_continuous_distributions(df, columns_to_plot=None):
             alpha=0.7,
             edgecolor='black'
             )
+        # on top of histrogram curve type plot also needed and this curve can be
+        # plotted using scientisific & statistical library of python "Scipy"
+        # gaussian_kde() calculates a smooth probability curve 
+        # KDE = Kernel Density Estimation
+        # using numpy array, creates 200 evenly-spaced points from min to max value
+        # again i=row in the grid while 0 is column of the grid
+        # loop need to move row and col wise for each column curve plot
+        # data or values need to draw this dotted line curve is again values from each column
+        # and which is under the loop df[column].dropna()
         kde = stats.gaussian_kde(data)
         x_values = np.linspace(data.min(), data.max(), 200)
         axes[i, 0].plot(
@@ -53,6 +73,8 @@ def plot_continuous_distributions(df, columns_to_plot=None):
             )
         axes[i, 0].set_title(f"{column} Histogram + KDE")
 
+        # this represent the right side plot in the grid
+        # box plot normally use percentile to represent the data in each col under loop
         axes[i, 1].boxplot(data, vert=False)
         axes[i, 1].set_title(f"{column} Boxplot")
 
