91cdf0689d9237a528cdb46ecccfdc363e6919d3
nasirk007 <nsr.capital.007@outlook.com>
2026-07-24 16:53:32 +0200

    heatmap plot to see correlation btw numerical column of DF, use seabon library built on top of matplot to visualisation of statistical analysis

--- data_analysis/data_preparation_visualization/9-plot_correlation_heatmap.py
+++ data_analysis/data_preparation_visualization/9-plot_correlation_heatmap.py
@@ -8,28 +8,18 @@ import matplotlib.pyplot as plt
 
 def plot_correlation_heatmap(df):
     """
-    Plots correlatoin heatmap of numerical features.
-    1. Input
-    Receive df as the function argument.
-    2. Compute correlations
-    Use DataFrame method, computes pairwise correlations across
-     numeric columns & Result: a square correlation matrix.
-    3. Plot heatmap
-    Use visualization function that accepts matrix/dataframe,
-    & draws color grid. Enable annotations so each cell shows
-    its numeric correlation value. Choose coolwarm colormap then.
-    Fix the value range with vmin=-1 and vmax=1.
-    4. Format appearance
-    Add a title like Correlation Matrix.
-    Ensure x and y labels are the feature names.
-    Keep the plot aspect oriented like the reference image.
-    5. Display and return
-    Show the plot.
-    Return None.
+    Plots a correlation heatmap for numeric columns.
     """
     plt.figure(figsize=(6, 5))
-    corr = df.corr(numeric_only=True)
-    sns.heatmap(data=corr, vmax=1, vmin=-1, cmap="coolwarn", annot=True)
+    numeric_df = df.select_dtypes(include="number")
+    corr = numeric_df.corr()
+    sns.heatmap(
+        corr,
+        annot=True,
+        cmap="coolwarm",
+        vmin=-1,
+        vmax=1
+        )
     plt.title("Correlation Matrix")
     plt.show()
     return None
