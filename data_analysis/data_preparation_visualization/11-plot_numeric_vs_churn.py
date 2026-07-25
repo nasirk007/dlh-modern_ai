3aca9427bb67c7ae1ba7705fec33d6b771fedac7
nasirk007 <nsr.capital.007@outlook.com>
2026-07-25 22:09:03 +0200

    filtering numerical values based on whether customer churned (Yes) or not (No)

--- data_analysis/data_preparation_visualization/11-plot_numeric_vs_churn.py
+++ data_analysis/data_preparation_visualization/11-plot_numeric_vs_churn.py
@@ -2,7 +2,6 @@
 """
 this module visualizes churn rate per continous numerical features.
 """
-import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -15,14 +14,6 @@ def plot_numeric_vs_churn(df, col):
     so you can visually compare where churners concentrate.
     """
     plt.figure(figsize=(12, 8))
-    # Data grouping/filtering: split column into two arrays using
-    # df["Churn"]=="Yes" & == "No" so each histogram uses only its subgroup.
-    # Histogram: counts(ordensity) of values in bins across numeric col.
-    # Overlap reveals where one class concentrates relative to the other.
-    # Density vs counts: use density=True to compare shapes (probability per
-    # bin) even if group sizes differ; otherwise use counts but be
-    # aware of different sample sizes.
-    # Transparency(alpha): let both histograms be visible.
     Yes_data = df[df["Churn"] == "Yes"][col]
     No_data = df[df["Churn"] == "No"][col]
     plt.hist([Yes_data, No_data], bins=30, label=["Yes", "No"])
