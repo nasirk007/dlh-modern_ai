d9f800aa2ac8cf4672cfc29448429d547eda6bfb
nasirk007 <nsr.capital.007@outlook.com>
2026-07-26 22:49:56 +0200

    creating new col namely number of service and grouping no of service over tenure groups

--- data_analysis/data_preparation_visualization/14-create_features.py
+++ data_analysis/data_preparation_visualization/14-create_features.py
@@ -9,16 +9,17 @@ def create_features(df):
     """Create new features based on existing ones.
     """
     df = df.drop(columns=["PhoneService"])
-    df["NumService"] = 0
-    service_cols = ["MultipleLines", "InternetService", "OnlineSecurity", "OnlineBackup",
-                    "DeviceProtection", "TechSupport", "StreamingTV",
-                    "StreamingMovies"]
+    df["NumServices"] = 0
+    service_cols = ["MultipleLines", "InternetService", "OnlineSecurity",
+                    "OnlineBackup", "DeviceProtection", "TechSupport",
+                    "StreamingTV", "StreamingMovies"]
     for col in service_cols:
         if col == "InternetService":
-            df["NumService"] += df[col].map({"DSL": 1,
+            df["NumServices"] += df[col].map({"DSL": 1,
                                              "Fiber optic": 1, "No": 0})
         else:
-            df["NumService"] += df[col].apply(lambda x: 1 if x == "Yes" else 0)
+            df["NumServices"] += df[col].apply(
+                lambda x: 1 if x == "Yes" else 0)
 
     df["TenureGroup"] = pd.cut(df["tenure"],
                                bins=[0, 12, 24, 48, 60, float("inf")],
