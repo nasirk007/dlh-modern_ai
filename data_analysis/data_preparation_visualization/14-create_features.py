b5b6f94a1059ee9f98ec7f9c03d6baaa2c45aabc
nasirk007 <nsr.capital.007@outlook.com>
2026-07-26 23:07:31 +0200

    creating new col namely number of service and grouping no of service over tenure groups

--- data_analysis/data_preparation_visualization/14-create_features.py
+++ data_analysis/data_preparation_visualization/14-create_features.py
@@ -1,3 +1,4 @@
+# %%
 #!/usr/bin/env python3
 """
 Module for creating new features from existing ones in a DataFrame.
@@ -8,23 +9,31 @@ import pandas as pd
 def create_features(df):
     """Create new features based on existing ones.
     """
-    df["NumServices"] = 0
-    service_cols = ["MultipleLines", "InternetService", "OnlineSecurity",
-                    "OnlineBackup", "DeviceProtection", "TechSupport",
+    # create an empty column for number of services
+    df["NumService"] = 0
+    # create a new feature for number of services
+    service_cols = ["MultipleLines", "OnlineSecurity", "OnlineBackup",
+                    "InternetService", "DeviceProtection", "TechSupport",
                     "StreamingTV", "StreamingMovies"]
     for col in service_cols:
         if col == "InternetService":
-            df["NumServices"] += df[col].map({"DSL": 1,
+            df["NumService"] += df[col].map({"DSL": 1,
                                              "Fiber optic": 1, "No": 0})
         else:
-            df["NumServices"] += df[col].apply(
+            df["NumService"] += df[col].apply(
                 lambda x: 1 if x == "Yes" else 0)
-
+    # create a new col as tenuregroup to dataframe
+    # use pd.cut to create bins for tenure
     df["TenureGroup"] = pd.cut(df["tenure"],
                                bins=[0, 12, 24, 48, 60, float("inf")],
                                labels=[
-                                   '0-12', '13-24', '25-48', '49-60', '60+'
-                                   ],
+                                   '0-12', '13-24', '25--48', '49-60', '60+'],
                                right=True)
-    df = df.drop(columns=service_cols + ["tenure"])
+    # labling was giving an idea to create bin for tenure like
+    # 0, 12, 24 .........as it was not explicitly given
+    # secondly this can be confirm that how many bins we may needed
+    # df["tenure"].value_counts()
+    # labling will replace internal bin index (0, 1..) with above strings
+    # setting right at true means interval close on the right side
+    df.drop(columns=service_cols + ["tenure"], inplace=True)
     return df
