039d9424a205dd89fb9951f408f85bcea0d3e0a7
nasirk007 <nsr.capital.007@outlook.com>
2026-07-26 02:55:11 +0200

    using scipy library for find p-value for categorial feature (column with categorial data). It also require an important pandas method crosstab to built table btw two categorial col first

--- data_analysis/data_preparation_visualization/12-chi_square_tests.py
+++ data_analysis/data_preparation_visualization/12-chi_square_tests.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python3
-"""assess independence of categorial feature from churn.
+"""
+This module performs chi-square tests for categorical features, using SciPy.
 """
 import pandas as pd
 from scipy import stats
 
 
 def chi_square_tests(df):
-    """Perform chi-square tests for categorical features against Churn.
+    """
+    Computes chi-square p-values
+for categorical columns against Churn
     """
     dictionary = {}
     for feature in df.columns:
-        if feature == "Churn":
-            continue
-        table = pd.crosstab(df[feature], df["Churn"])
-        test_result = stats.chi2_contingency(table)
-        chi2 = test_result[0]
-        p_value = test_result[1]
-        dof = test_result[2]
-        expected = test_result[3]
-        dictionary[feature] = p_value
+        if df[feature].dtype == "object" and feature != "Churn":
+            table = pd.crosstab(df[feature], df["Churn"])
+
+            test_result = stats.chi2_contingency(table)
+
+            chi2 = test_result[0]
+            p_value = test_result[1]
+            dof = test_result[2]
+            expected = test_result[3]
+
+            dictionary[feature] = p_value
     return dictionary
