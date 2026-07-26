faf2de31b847d2ce8409e4756308bb0ccbfe37df
nasirk007 <nsr.capital.007@outlook.com>
2026-07-26 05:14:31 +0200

    ttest using scipy library function ttest_ind for group/2-sample ttest

--- data_analysis/data_preparation_visualization/13-ttest_numeric.py
+++ data_analysis/data_preparation_visualization/13-ttest_numeric.py
@@ -11,12 +11,9 @@ def ttest_numeric(df):
     Perform t-tests for over numerical data/features.
     three different ttest exist, one sample, two-sample
     and paired ttest.
-    compare one sample mean to know pop mean
-    compare mean of two separate groups
-    compared
     large ttest value means, larger diff btw means
     and p-value will confirm whether this diff is
-    statistical significant
+    statistical significant or not.
     """
     dictionary = {}
     for feature in df.columns:
@@ -25,14 +22,5 @@ def ttest_numeric(df):
             No_Group = df[df["Churn"] == "No"][feature].dropna()
             statistic, p_value = stats.ttest_ind(
                 Yes_Group, No_Group, equal_var=False)
-            # t-test is used when sample size is less than 30 or when
-            # population std deviation is unknown, and it uses sample
-            # std deviation and adj with uncertainty
-            # while z-test rely on ND and used population
-            # std deviation in any case
-            # eqaul variance assumption of ttest, means variance btw
-            # two group are not equal and make it explicit by "False"
-            # test will compare mean of Yes_Group with No_Group
-            # rather mean of Churn col with mean of tenure col
             dictionary[feature] = p_value
     return dictionary
