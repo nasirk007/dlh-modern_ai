5be669943154ed7d139118b5813a12686e297897
nasirk007 <nsr.capital.007@outlook.com>
2026-07-26 05:18:08 +0200

    ttest using scipy library function ttest_ind for group/2-sample ttest

--- data_analysis/data_preparation_visualization/13-ttest_numeric.py
+++ data_analysis/data_preparation_visualization/13-ttest_numeric.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """
-This module performs chi-square tests for categorical features.
+This module performs chi-square tests for categorical features, using SciPy.
 """
+import pandas as pd
 from scipy import stats
 
 
@@ -10,9 +11,12 @@ def ttest_numeric(df):
     Perform t-tests for over numerical data/features.
     three different ttest exist, one sample, two-sample
     and paired ttest.
+    compare one sample mean to know pop mean
+    compare mean of two separate groups
+    compared
     large ttest value means, larger diff btw means
     and p-value will confirm whether this diff is
-    statistical significant or not.
+    statistical significant
     """
     dictionary = {}
     for feature in df.columns:
@@ -21,5 +25,14 @@ def ttest_numeric(df):
             No_Group = df[df["Churn"] == "No"][feature].dropna()
             statistic, p_value = stats.ttest_ind(
                 Yes_Group, No_Group, equal_var=False)
+            # t-test is used when sample size is less than 30 or when
+            # population std deviation is unknown, and it uses sample
+            # std deviation and adj with uncertainty
+            # while z-test rely on ND and used population
+            # std deviation in any case
+            # eqaul variance assumption of ttest, means variance btw
+            # two group are not equal and make it explicit by "False"
+            # test will compare mean of Yes_Group with No_Group
+            # rather mean of Churn col with mean of tenure col
             dictionary[feature] = p_value
     return dictionary
