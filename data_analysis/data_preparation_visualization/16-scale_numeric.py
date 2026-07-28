f552d96ffe9817b852978caaf91b98d758fd3e21
nasirk007 <nsr.capital.007@outlook.com>
2026-07-29 01:31:44 +0200

    monthly and total charges contain number beyond 0 and 1 while rest of the data converted into 0 & 1, so for such numercial variable(columns) and each value can be converted into 0 and 1 using scalling approach which is just storing original numercial values (beyond 0 & 1) into thier 0 & 1 equivalance and this equivalence is large in decimal points e.g. in row 0, monthly charges was 29.85 and after scaling it is -1.161694

---
+++ data_analysis/data_preparation_visualization/16-scale_numeric.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+this module contains functions to scale numeric variables
+(columns with int/float type) into numerical value using
+scikit-learn, library famous for ML work using python.
+"""
+import pandas as pd
+from sklearn import preprocessing
+
+
+def scale_numeric(df):
+    """
+    Scale numeric variables in a DataFrame using StandardScaler.
+    Scale features to a common range or distribution, important for
+    many ML algorithms sensitive to feature magnitudes.
+    There are two type of scalling one is normal and other is standard.
+    Normalization is used to scale the data between 0 and 1, while
+    standardization is used to scale the data to have a mean of 0
+    and a standard deviation of 1.
+    Transforms features to have mean = 0 and standard deviation = 1,
+    useful for normally distributed features.
+    feature means categorial datatype columns excluding target variable
+    ("Churn") which is also categorical datatype column.
+    """
+    std_scaler = preprocessing.StandardScaler()
+    df[["MonthlyCharges", "TotalCharges"]] = std_scaler.fit_transform(
+        df[["MonthlyCharges", "TotalCharges"]])
+    return df
