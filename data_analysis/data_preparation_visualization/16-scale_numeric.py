f887c54e9ca36fe3f1d38dfa7a752920adeac8e2
nasirk007 <nsr.capital.007@outlook.com>
2026-07-29 01:33:02 +0200

    monthly and total charges contain number beyond 0 and 1 while rest of the data converted into 0 & 1, so for such numercial variable(columns) and each value can be converted into 0 and 1 using scalling approach which is just storing original numercial values (beyond 0 & 1) into thier 0 & 1 equivalance and this equivalence is large in decimal points e.g. in row 0, monthly charges was 29.85 and after scaling it is -1.161694

--- data_analysis/data_preparation_visualization/16-scale_numeric.py
+++ data_analysis/data_preparation_visualization/16-scale_numeric.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """
 this module contains functions to scale numeric variables
-(columns with int/float type) into numerical value using
-scikit-learn, library famous for ML work using python.
+into numerical value using scikit-learn, library.
 """
 import pandas as pd
 from sklearn import preprocessing
