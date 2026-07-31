09f0907666f51d4faa92b7b41c8da641e0ffb46d
nasirk007 <nsr.capital.007@outlook.com>
2026-07-31 22:40:37 +0200

    regression evaluation metrics

--- machine_learning/linear_models/1-regression_evaluation_metrics.py
+++ machine_learning/linear_models/1-regression_evaluation_metrics.py
@@ -11,8 +11,8 @@ def evaluation_metrics_for_regression(y_true, y_pred):
     including mean absolute error, mean squared error, 
     root mean squared error and R Square.
     """
-    mae = metrics.mean_absolute_error(y_true, y_pred)
     mse = metrics.mean_squared_error(y_true, y_pred)
     rmse = metrics.root_mean_squared_error(y_true, y_pred)
+    mae = metrics.mean_absolute_error(y_true, y_pred)
     r2 = metrics.r2_score(y_true, y_pred)
-    return (mae, mse, rmse, r2)
+    return (mae, rmse, mse, r2)
