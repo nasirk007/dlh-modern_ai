1ce7fe951c47659b74317bc475c6d6c342180025
nasirk007 <nsr.capital.007@outlook.com>
2026-08-01 05:33:53 +0200

    ridge regression L1, add penalty to loss function or MSE/RSS

--- machine_learning/linear_models/2-ridge_regression copy.py
+++ machine_learning/linear_models/2-ridge_regression copy.py
@@ -7,18 +7,6 @@ from sklearn import linear_model
 
 
 def ridge_regression(random_state):
-    """to tune regression model using ridge regression.
-    ridge regression is a regularization technique that adds
-    a penalty term to the linear regression loss/cost function,
-    which helps to prevent overfitting and improve
-    the model's generalization performance.
-    Ridge regression is particularly useful for overfitted problems
-    or when coefficients are large in magnitude, and
-    computation for model become complex. to reduce this complexity,
-    ridge regression adds a penalty term to the cost/loss function.
-    this loss or cost function is simply the error and more precisely
-    its either residual sum of squares (RSS) (from adj R2) or
-    mean squared error (MSE).
-    """
+    """to tune regression model using ridge regression."""
     model = linear_model.Ridge(random_state=random_state)
     return model
