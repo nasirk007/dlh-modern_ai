9e0446d76d765522fcf3065ec36a954cb272ee73
nasirk007 <nsr.capital.007@outlook.com>
2026-08-02 02:33:37 +0200

    support vector machine, a regression model

--- machine_learning/linear_models/6-svm.py
+++ machine_learning/linear_models/6-svm.py
@@ -6,5 +6,6 @@ from sklearn import svm
 
 def get_SVM_model(name, random_state):
     """to tune regression model using support vector machine."""
-    model = svm.SVC(random_state=random_state)
+    model = svm.SVC(kernel=['linear', 'poly', 'rbf'],
+                    random_state=random_state)
     return model
