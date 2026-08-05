d4fd0d3ef7850aeb1c3c5c45d5094512c68b548e
nasirk007 <nsr.capital.007@outlook.com>
2026-08-05 21:06:15 +0200

    building decision tree model

---
+++ machine_learning/tree_models/0-build.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Module for decision tree model building."""
+from sklearn import tree
+
+
+def build_decision_tree(min_samples_leaf, min_samples_split, random_state):
+    """
+    Build a decision tree model using the training data.
+
+    Parameters: integer
+    min_samples_leaf: mini no of samples required to be at a leaf node.
+    min_samples_split: mini no of samples required to split an internal node.
+    random_state: The seed used by the random number generator.
+    """
+    # Create a decision tree classifier
+    model = tree.DecisionTreeClassifier(
+        min_samples_leaf=min_samples_leaf,
+        min_samples_split=min_samples_split,
+        random_state=random_state
+    )
+    return model
