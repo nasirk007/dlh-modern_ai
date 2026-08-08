bc17c7d56597d88559d9024e16a433f152fca3a1
nasirk007 <nsr.capital.007@outlook.com>
2026-08-08 02:29:53 +0200

    using trained model to predict

---
+++ machine_learning/tree_models/3-generate_predictions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module to do prediction with a decision tree model."""
+from sklearn import tree
+
+
+def generate_predictions(clf, X):
+    """Make predictions using a trained decision tree classifier.
+    Parameters:
+    clf: A trained Scikit-learn classifier instance
+    X: Feature matrix (NumPy array or pandas DataFrame)
+    Return: A NumPy array containing the predicted class
+    labels for the input samples.
+    """
+    predictions = clf.predict(X)
+    return predictions
