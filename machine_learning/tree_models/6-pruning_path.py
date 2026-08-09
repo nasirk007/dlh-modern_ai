6b1b5d7d1dd34613399332e36b80b859fdd8c9c9
nasirk007 <nsr.capital.007@outlook.com>
2026-08-09 06:10:41 +0200

    post pruning using cost-complexity method and ccp_alpha parameter

--- machine_learning/tree_models/6-pruning_path.py
+++ machine_learning/tree_models/6-pruning_path.py
@@ -16,8 +16,18 @@ def get_pruning_path(clf, X, y):
     values used for pruning (post prunning).
     impurities: A NumPy array containing the total impurity of
     leaves at each corresponding alpha value.
+
+    alpha is simply a penalty to complexity of tree.
+    ccp_alphas is a hyperparameter that controls the complexity of
+    the model after being built this task is analyse the trade-off
+    btw ccp_alpha and the accuracy of the model.
+    The higher the ccp_alpha, the model pruned more which can lead to
+    a simpler model with less overfitting. However, if ccp_alpha is
+    too high, the model may become too simple and underfit.
+    larger alpha penalises the complexity of the model more,
+    leading to a simpler model. In the model each node has alpha
+    value and node with the lowest alpha value is pruned first.
     """
-    # retrieve the cost-complexity pruning path
     path = clf.cost_complexity_pruning_path(X, y)
     ccp_alphas, impurities = path.ccp_alphas, path.impurities
     return ccp_alphas, impurities
