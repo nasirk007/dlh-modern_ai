fec074650178d8fbb1145b6ff34eef490a7554b7
nasirk007 <nsr.capital.007@outlook.com>
2026-08-10 07:59:11 +0200

    argsort numpy function to return sorted indicies/position of each feature with importance

--- machine_learning/tree_models/10-feature_importance.py
+++ machine_learning/tree_models/10-feature_importance.py
@@ -19,5 +19,14 @@ def feature_importance(rf):
         to most important (ascending order).
     """
     importances = rf.feature_importances_
+    # when we print importannces, we will see list of floats
+    # for each feature/column but argsort is nuumpy function, will
+    # returns the positions (indices) that would sort the importance values.
+    # index == feature name == importance values
+    # argsort function will look into importance values, then will
+    # sort them from lowest to highest. In this exercise we have 13 features
+    # and correspinding 13 importance values. let say at index 7 we have 0.0057
+    # a lowest value, when sorted it will be at index 0, so argsort will return
+    # 7 at index 0. and this is output of argsort function in total.
     indices = np.argsort(importances)
     return importances, indices
