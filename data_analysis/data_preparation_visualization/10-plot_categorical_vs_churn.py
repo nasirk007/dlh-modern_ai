6252423708cfae304306bf6e69524b5fbe494d89
nasirk007 <nsr.capital.007@outlook.com>
2026-08-15 10:04:02 +0200

    updating with notes

--- data_analysis/data_preparation_visualization/10-plot_categorical_vs_churn.py
+++ data_analysis/data_preparation_visualization/10-plot_categorical_vs_churn.py
@@ -12,29 +12,34 @@ def plot_categorical_vs_churn(df, col):
     use the full DataFrame, and
     group it by the categorical column, e.g in gender 2 categories
     exist, male & female, group all rows of male and female
-    separately, the count total female rows (3500), and male rows 
+    separately, the count total female rows (3500), and male rows
     (3532) in the same gender column, in 3500 female group, 700
     include yes in churn column, so 700/3500 = 20% female churn
     likewise, out of total males 1300/3532 = 40% male churned
-    in the same fashion, analyse phone service column 
+    in the same fashion, analyse phone service column
     compute churn proportion per group,
     plot that proportion as a bar chart.
     """
-    # churn rate/category = no of yes row in each group / total row in that group
-    # group means e.g. under gender col, male and female are two separate groups
-    # mean() is used to compute proportion of yes values in each group 
+    # churn rate/category = no of yes row in each
+    # group / total row in that group
+    # group means e.g. under gender col, male and female
+    # are two separate groups, mean() is used to
+    # compute proportion of yes value in each group
     plt.figure(figsize=(12, 8))
-    churn_rate = df.groupby(col)["Churn"].apply(lambda x: (x == "Yes").mean())
+    churn_rate = df.groupby(col)["Churn"].apply(
+        lambda x: (x == "Yes").mean())
     plt.bar(churn_rate.index, churn_rate.values)
     plt.ylabel("Churn Rate")
     plt.xticks(rotation=45)
     plt.title(f"Churn Rate by {col}")
     plt.show()
     # The figure shows a churn rate for gender:
-    # Male: ~26.2% = Female: ~26.95% ,showing no signal for the column.
-    # After running further analysis we will validate the removal of this column.
+    # Male: ~26.2% = Female: ~26.95% ,showing no signal for col.
+    # After running further analysis we will validate
+    # the removal of this column.
     # The figure shows a churn rate for PhoneService:
-    # No: ~25% and Yes: ~26.74% Showing no signal for the column, indicating a very
-    # small difference and suggesting that this feature shows no
-    # strong signal for predicting churn.
+    # No: ~25% and Yes: ~26.74% Showing no signal for the
+    # column, indicating a very small difference and
+    # suggesting that this feature show no strong
+    # signal for predicting churn.
     return None
