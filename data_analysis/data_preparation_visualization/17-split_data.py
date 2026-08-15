973a34862bf2b2b6b985a4160f8f764f0a048fc3
nasirk007 <nsr.capital.007@outlook.com>
2026-08-15 10:52:45 +0200

    updating with notes

--- data_analysis/data_preparation_visualization/17-split_data.py
+++ data_analysis/data_preparation_visualization/17-split_data.py
@@ -12,7 +12,7 @@ def split_data(df, target='Churn', test_size=0.2, random_state=42):
     Y = df[target]
     X_train, X_test, y_train, y_test = model_selection.train_test_split(
         X, Y, test_size=test_size, random_state=random_state)
-    # chrun was encoded through lable encoder, values are 0 & 1 now 
+    # chrun was encoded through lable encoder, values are 0 & 1 now
     # and X_train.mean() will give you % customer churn rate in
     # train data and so on........ see checker 17.main.py file
     return X_train, X_test, y_train, y_test
