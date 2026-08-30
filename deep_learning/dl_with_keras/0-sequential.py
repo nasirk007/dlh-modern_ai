54d977372b22d82245989a82712b4885cd39d5f8
nasirk007 <nsr.capital.007@outlook.com>
2026-08-30 20:01:39 +0200

    Sequential model where input_dim define inside hidden layer

--- deep_learning/dl_with_keras/0-sequential.py
+++ deep_learning/dl_with_keras/0-sequential.py
@@ -15,7 +15,7 @@ def build_model(input_dim, neurons_h):
         [
             keras.layers.Dense(
                 neurons_h,
-                activation='Sigmoid',
+                activation='sigmoid',
                 input_shape=(input_dim,)  # Must be a tuple
             ),
             keras.layers.Dense(10, activation='softmax')
