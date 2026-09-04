4c5718dff1f68ce2c4115b90b03bb28120e54a73
nasirk007 <nsr.capital.007@outlook.com>
2026-09-04 03:40:06 +0200

    reset 101

---
+++ deep_learning/cv_architectures/3-data_aug.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Literaly build a reusable augmentation pipe. Pip-pip!
+"""
+import tensorflow as tf
+
+
+def build_data_augmentation():
+    """
+    Return a seeded Sequential image augumentation model
+    """
+
+    seed = 42
+    return tf.keras.Sequential([
+        tf.keras.layers.RandomFlip(
+            'horizontal',
+            seed=seed
+        ),
+        tf.keras.layers.RandomRotation(
+            0.15,
+            seed=seed
+        ),
+        tf.keras.layers.RandomZoom(
+            0.15,
+            seed=seed
+        ),
+        tf.keras.layers.RandomContrast(
+            0.1,
+            seed=seed
+        )
+    ])
