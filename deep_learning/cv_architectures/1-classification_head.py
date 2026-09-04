9389afd30eaffc39f5b0eac95653822a248dd411
nasirk007 <nsr.capital.007@outlook.com>
2026-09-04 03:37:07 +0200

    Train CNN

---
+++ deep_learning/cv_architectures/1-classification_head.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Add a little head (dikson_puaro)
+on top of one with the brains (feature extractor)
+"""
+from tensorflow import keras
+
+
+def add_classification_head(
+        base_model,
+        num_classes
+):
+    """
+    Add hidden and output classification
+    layers to a feature extractor
+    """
+    inputs = keras.Input(
+        shape=base_model.input_shape[1:]
+    )
+    features = base_model(inputs)
+    hidden = keras.layers.Dense(
+        128,
+        activation='relu'
+    )(features)
+    outputs = keras.layers.Dense(
+        num_classes,
+        activation='softmax'
+    )(hidden)
+
+    return keras.Model(inputs, outputs)
